465da0af33eecad5687d566abc82b5bb88278498
ssoteloserrano <ssoteloserrano@gmail.com>
2022-09-28 21:24:05 -0600

    Add lua colorizer plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -142,7 +142,7 @@ return packer.startup(function(use)
 
   use 'kdheepak/lazygit.nvim'
 
-  use 'ap/vim-css-color'
+  use 'norcalli/nvim-colorizer.lua'
 
   if PACKER_BOOTSTRAP then
       require("packer").sync()
