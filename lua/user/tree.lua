1dd0d7357d3403d648cbf6958d57422477063d35
ssoteloserrano <ssoteloserrano@gmail.com>
2022-10-15 22:03:45 -0600

    Comment calling to deprecated nvim tree resize function

--- lua/user/tree.lua
+++ lua/user/tree.lua
@@ -117,24 +117,24 @@ nvim_tree.setup({
   },
 })
 
-local nvim_tree_events = require('nvim-tree.events')
-local bufferline_state = require('bufferline.state')
+-- local nvim_tree_events = require('nvim-tree.events')
+-- local bufferline_api = require('bufferline.api')
 
-local function get_tree_size()
-  return require'nvim-tree.view'.View.width
-end
+-- local function get_tree_size()
+--   return require'nvim-tree.view'.View.width
+-- end
 
-nvim_tree_events.subscribe('TreeOpen', function()
-  bufferline_state.set_offset(get_tree_size())
-end)
+-- nvim_tree_events.subscribe('TreeOpen', function()
+--   bufferline_api.set_offset(get_tree_size())
+-- end)
 
-nvim_tree_events.subscribe('Resize', function()
-  bufferline_state.set_offset(get_tree_size())
-end)
+-- nvim_tree_events.subscribe('Resize', function()
+--   bufferline_api.set_offset(get_tree_size())
+-- end)
 
-nvim_tree_events.subscribe('TreeClose', function()
-  bufferline_state.set_offset(0)
-end)
+-- nvim_tree_events.subscribe('TreeClose', function()
+--   bufferline_api.set_offset(0)
+-- end)
 
 -- Keybindings
 vim.keymap.set('n', '<leader>n', "<cmd>NvimTreeToggle<cr>", options)
