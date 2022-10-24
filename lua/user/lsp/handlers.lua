023cd1a231ba0b7d17d9dbc5ba21e937a2acecdf
scar <ssoteloserrano@gmail.com>
2022-10-23 18:09:07 -0600

    Fix concatenation error

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -95,7 +95,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local status_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
 if not status_ok then
-  print('ERROR: ' .. status_ok)
+  print(status_ok)
 else
   M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 end
