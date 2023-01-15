17909209e9d547f906e269f47e0b70ce619a7514
scarktt <ssoteloserrano@gmail.com>
2023-01-15 16:54:51 -0600

    Add log printer

--- lua/plugins-config/lsp/handlers.lua
+++ lua/plugins-config/lsp/handlers.lua
@@ -95,7 +95,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local status_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
 if not status_ok then
-  print(status_ok)
+  print('handlers:' .. tostring(status_ok))
 else
   M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 end
