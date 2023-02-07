85a4da15829ff212429815c2b2bce40ca82ad9ec
scarktt <ssoteloserrano@gmail.com>
2023-02-06 21:00:15 -0600

    Add tree opening at neovim startup options

--- lua/plugins-config/tree.lua
+++ lua/plugins-config/tree.lua
@@ -112,25 +112,29 @@ nvim_tree.setup({
   },
 })
 
--- local nvim_tree_events = require('nvim-tree.events')
--- local bufferline_api = require('bufferline.api')
+local function open_nvim_tree(data)
 
--- local function get_tree_size()
---   return require'nvim-tree.view'.View.width
--- end
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
 
--- nvim_tree_events.subscribe('TreeOpen', function()
---   bufferline_api.set_offset(get_tree_size())
--- end)
+  if not directory then
+    return
+  end
 
--- nvim_tree_events.subscribe('Resize', function()
---   bufferline_api.set_offset(get_tree_size())
--- end)
+  -- create a new, empty buffer
+  vim.cmd.enew()
 
--- nvim_tree_events.subscribe('TreeClose', function()
---   bufferline_api.set_offset(0)
--- end)
+  -- wipe the directory buffer
+  vim.cmd.bw(data.buf)
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 -- Keybindings
 vim.keymap.set('n', '<leader>n', "<cmd>NvimTreeToggle<cr>", options)
-
