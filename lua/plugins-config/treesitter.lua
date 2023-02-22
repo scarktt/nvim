3af20f6e2ac106696146c5810bdabb34a6286d36
scarktt <ssoteloserrano@gmail.com>
2023-02-22 08:59:47 -0600

    Add php to ensure install config on windows

--- lua/plugins-config/treesitter.lua
+++ lua/plugins-config/treesitter.lua
@@ -8,23 +8,23 @@ local ensure_installed = {}
 
 if string.find(os_name, "unix") then
   ensure_installed = { "c", "lua", "vim", "help", "python", "javascript", "vue", "css", "html", "json", "markdown",
-    "regex", "vim", "yaml", "http", "php" }
+      "regex", "vim", "yaml", "http", "php" }
 else
-    -- windows path
+  -- windows path
   ensure_installed = { "c", "lua", "vim", "help", "python", "javascript", "css", "json", "markdown",
-    "regex", "vim", "http" }
+      "regex", "vim", "http", "php" }
 end
 
 treesitter.setup({
-  ensure_installed = ensure_installed,
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
-  indent = { enable = false },
-  autopairs = { enable = true },
-  autotag = { enable = true },
+    ensure_installed = ensure_installed,
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+    },
+    indent = { enable = false },
+    autopairs = { enable = true },
+    autotag = { enable = true },
 })
 
 local status_ok, compilers_install = pcall(require, "nvim-treesitter.install")
@@ -33,11 +33,11 @@ if status_ok then
   compilers_install.compilers = { "clang", "gcc" }
 end
 
-vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
-  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
-  callback = function()
-    vim.opt.foldmethod     = 'expr'
-    vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
-    vim.o.foldenable       = false -- Disable folding at startup
-  end
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufAdd', 'BufNew', 'BufNewFile', 'BufWinEnter' }, {
+    group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+    callback = function()
+      vim.opt.foldmethod = 'expr'
+      vim.opt.foldexpr   = 'nvim_treesitter#foldexpr()'
+      vim.o.foldenable   = false -- Disable folding at startup
+    end
 })
