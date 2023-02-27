570d1baf5ac2ea10a88e26d30d0cd65bc827b222
scarktt <ssoteloserrano@gmail.com>
2023-02-26 22:21:16 -0600

    add transparent background

--- lua/plugins-config/colorschemes/init.lua
+++ lua/plugins-config/colorschemes/init.lua
@@ -1,23 +1,31 @@
--- local is_ok, transparent = pcall(require, "transparent")
--- if not is_ok then
--- 	return
--- end
-
--- transparent.setup({
---   enable = true, -- boolean: enable transparent
---   extra_groups = { -- table/string: additional groups that should be cleared
---     -- In particular, when you set it to 'all', that means all available groups
-
---     -- example of akinsho/nvim-bufferline.lua
---     "BufferLineTabClose",
---     "BufferlineBufferSelected",
---     "BufferLineFill",
---     "BufferLineBackground",
---     "BufferLineSeparator",
---     "BufferLineIndicatorSelected",
---   },
---   exclude = {}, -- table: groups you don't want to clear
--- })
+local is_ok, transparent = pcall(require, "transparent")
+if not is_ok then
+	return
+else
+  transparent.setup({
+    enable = true, -- boolean: enable transparent
+    extra_groups = { -- table/string: additional groups that should be cleared
+      -- example of akinsho/nvim-bufferline.lua
+      "BufferLineTabClose",
+      "BufferlineBufferSelected",
+      "BufferLineFill",
+      "BufferLineBackground",
+      "BufferLineSeparator",
+      "BufferLineIndicatorSelected",
+      "NvimTreeNormal",
+      "NvimTreeStatuslineNc",
+      "NvimTreeWindowPicker",
+      "NvimTreeSymlink",
+      "NvimTreeEndOfBuffer", 
+      "NvimTreeCursorLine",
+      "NvimTreeCursorLineNr",
+      "NvimTreeLineNr",
+      "NvimTreeWinSeparatorend",
+      "NvimTreeCursorColumn",
+    },
+    exclude = {}, -- table: groups you don't want to clear
+  })
+end
 
 -- require("user.colorschemes.nightfox")
 require("plugins-config.colorschemes.everforest")
