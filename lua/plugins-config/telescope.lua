4716dac305013e0d9777bfa6a950e4b5a62829ae
scarktt <ssoteloserrano@gmail.com>
2023-03-11 18:01:56 -0600

    feat: add new telescope keymaps

--- lua/plugins-config/telescope.lua
+++ lua/plugins-config/telescope.lua
@@ -3,8 +3,8 @@ if not ok then
   return
 end
 
-local sorters, actions, previewers = require('telescope.sorters'), require('telescope.actions'),
-    require('telescope.previewers')
+local sorters, actions, previewers, builtin = require('telescope.sorters'), require('telescope.actions'),
+    require('telescope.previewers'), require('telescope.builtin')
 
 local rip_grep_config = {
   'rg',
@@ -61,6 +61,11 @@ telescope.setup {
         },
       },
     },
+    pickers = {
+      colorscheme = {
+        enable_preview = true
+      }
+    }
   },
   extensions = {
     fzy_native = {
@@ -77,7 +82,28 @@ telescope.load_extension('file_browser')
 
 -- telescope.load_extension("projects")
 
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap('n', '<Leader>ff', '<cmd>Telescope find_files<cr>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>fb', '<cmd>Telescope buffers<cr>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>w', '<cmd>Telescope live_grep<cr>', opts)
+-- Find files from current file's project
+-- using .git as a project root
+vim.keymap.set('n', 'ff', function()
+  local opts = opts or {}
+  opts.cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+  if vim.v.shell_error ~= 0 then
+    -- if not git then active lsp client root
+    -- will get the configured root directory of the first attached lsp. You will have problems if you are using multiple lsps
+    opts.cwd = vim.lsp.get_active_clients()[1].config.root_dir
+  end
+  builtin.find_files(opts)
+end, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<Leader>fb', builtin.buffers, { desc = '[F]ind opened [B]uffers' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 15,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+vim.keymap.set('n', '<Leader>r', builtin.oldfiles, { desc = '[R]ecently opened files' })
+vim.keymap.set('n', '<Leader>w', builtin.live_grep, { desc = 'Find [W]ord' })
+vim.keymap.set('n', '<Leader>cs', builtin.colorscheme, { desc = '[C]olor [S]cheme' })
+vim.keymap.set('n', '<Leader>c', builtin.registers)
