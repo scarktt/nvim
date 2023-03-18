23fa8c1384a6bd22229f92baa1a0903463635d29
scarktt <ssoteloserrano@gmail.com>
2023-03-18 15:27:25 -0600

    chore: add custom sections and reorder sections

--- lua/plugins-config/lualine.lua
+++ lua/plugins-config/lualine.lua
@@ -3,9 +3,20 @@ if not lualine_status_ok then
   return
 end
 
-local function custom()
-  return [[力]]
-end
+local conditions = {
+  buffer_not_empty = function()
+    return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+  end,
+  hide_in_width = function()
+    return vim.fn.winwidth(0) > 80
+  end,
+  check_git_workspace = function()
+    local filepath = vim.fn.expand('%:p:h')
+    local gitdir = vim.fn.finddir('.git', filepath .. ';')
+    return gitdir and #gitdir > 0 and #gitdir < #filepath
+  end,
+}
+
 -- stylua: ignore
 local colors = {
   blue       = '#7DC4E4',
@@ -41,31 +52,94 @@ local scar_theme = {
   },
 }
 
+local user_name = {
+-- mode component
+  function()
+    return 'スカル'
+  end,
+  color = function()
+    -- auto change color according to neovims mode
+    local mode_color = {
+      n = colors.blue,
+      i = colors.green,
+      v = colors.violet,
+      [''] = colors.violet,
+      V = colors.violet,
+      c = colors.magenta,
+      no = colors.red,
+      s = colors.orange,
+      S = colors.orange,
+      [''] = colors.orange,
+      ic = colors.yellow,
+      R = colors.violet,
+      Rv = colors.violet,
+      cv = colors.red,
+      ce = colors.red,
+      r = colors.cyan,
+      rm = colors.cyan,
+      ['r?'] = colors.cyan,
+      ['!'] = colors.red,
+      t = colors.red,
+    }
+    return { fg = mode_color[vim.fn.mode()] }
+  end,
+  cond = conditions.hide_in_width,
+}
+
+local custom_diff =  {
+  'diff',
+  symbols = { added = ' ', modified = '柳', removed = ' ' },
+  diff_color = {
+    added = { fg = colors.green },
+    modified = { fg = colors.yellow },
+    removed = { fg = colors.red },
+  },
+  cond = conditions.hide_in_width,
+}
+
+local lsp = {
+  function()
+    local msg = 'No Active Lsp'
+    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then
+      return msg
+    end
+    for _, client in ipairs(clients) do
+      local filetypes = client.config.filetypes
+      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+        return client.name
+      end
+    end
+    return msg
+  end,
+  icon = ' lsp:',
+  cond = conditions.hide_in_width,
+}
+
 lualine.setup {
   options = {
     icons_enabled = true,
-    component_separators = { left = '', right = ''},
-    section_separators = { left = ' ', right = ''},
     theme = scar_theme,
     -- theme = 'auto',
+    component_separators = { left = "", right = "" },
+    section_separators = { left = ' ', right = ''},
     disabled_filetypes = { },
-    always_divide_middle = true,
-    -- disabled_filetypes = { 'packer', 'NvimTree' },
+    always_divide_middle = false,
   },
   sections = {
-    lualine_a = {'mode', custom},
-    -- lualine_a = {require('auto-session-library').current_session_name},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = { "encoding", "fileformat", "filetype" },
-    lualine_y = {},
-    lualine_z = {'location'}
+    lualine_a = {'mode'},
+    lualine_b = {user_name},
+    lualine_c = {lsp},
+    lualine_x = { 'filename', 'diagnostics' },
+    lualine_y = {custom_diff},
+    lualine_z = {'branch'}
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
     lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_x = {'diagnostics'},
     lualine_y = {},
     lualine_z = {}
   },
