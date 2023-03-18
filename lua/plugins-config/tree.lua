f9543dfe4a39ccd60c122a99148e31ada8a52c0f
scarktt <ssoteloserrano@gmail.com>
2023-03-18 15:27:56 -0600

    chore: change display side

--- lua/plugins-config/tree.lua
+++ lua/plugins-config/tree.lua
@@ -95,9 +95,8 @@ nvim_tree.setup({
   },
   view = {
     width = 30,
-    -- height = 30,
     hide_root_folder = false,
-    side = "left",
+    side = "right",
     -- auto_resize = true,
     mappings = {
       custom_only = false,
