af8565457b9dda585d9f182d414aac97a5d28887
scarktt <ssoteloserrano@gmail.com>
2023-03-18 15:15:54 -0600

    fix: find files on telescope from current project root

--- lua/plugins-config/telescope.lua
+++ lua/plugins-config/telescope.lua
@@ -80,26 +80,23 @@ telescope.load_extension('file_browser')
 
 telescope.load_extension("project")
 
--- Find files from current file's project
--- using .git as a project root
-vim.keymap.set('n', 'ff', function()
-  local opts = opts or {}
-  opts.cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
-  if vim.v.shell_error ~= 0 then
-    -- if not git then active lsp client root
-    -- will get the configured root directory of the first attached lsp. You will have problems if you are using multiple lsps
-    opts.cwd = vim.lsp.get_active_clients()[1].config.root_dir
-  end
+
+local find_files_from_root = function()
+  local opts = {}
+  opts.cwd = require('utils').get_project_root()
   builtin.find_files(opts)
-end, { desc = '[F]ind [F]iles' })
-vim.keymap.set('n', '<Leader>fb', builtin.buffers, { desc = '[F]ind opened [B]uffers' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
+end
+
+local find_files_in_current_buffer = function()
   builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 15,
     previewer = false,
   })
-end, { desc = '[/] Fuzzily search in current buffer' })
+end
+
+vim.keymap.set('n', 'ff', find_files_from_root, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<Leader>fb', builtin.buffers, { desc = '[F]ind opened [B]uffers' })
+vim.keymap.set('n', '<Leader>/', find_files_in_current_buffer, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<Leader>r', builtin.oldfiles, { desc = '[R]ecently opened files' })
 vim.keymap.set('n', '<Leader>w', builtin.live_grep, { desc = 'Find [W]ord' })
--- lua/utils.lua
+++ lua/utils.lua
@@ -4,4 +4,14 @@ function utils.get_current_os()
     return package.config:sub(1,1) == "\\" and "win" or "unix"
 end
 
+function utils.get_project_root()
+  local root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+  if vim.v.shell_error ~= 0 then
+    -- if not git then active lsp client root
+    -- will get the configured root directory of the first attached lsp. You will have problems if you are using multiple lsps
+    root = vim.lsp.get_active_clients()[1].config.root_dir
+  end
+  return root
+end
+
 return utils
