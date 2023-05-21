83bfd8046fe4e061d9be921e25cbb1990c232fa8
scarktt <ssoteloserrano@gmail.com>
2023-05-21 13:43:30 -0600

    feat(utils): add new utils functions for winbar

--- lua/utils.lua
+++ lua/utils.lua
@@ -14,4 +14,17 @@ function utils.get_project_root()
   return root
 end
 
+function utils.isempty(s)
+  return s == nil or s == ""
+end
+
+function utils.get_buf_option(opt)
+  local status_ok, buf_option = pcall(vim.api.nvim_buf_get_option, 0, opt)
+  if not status_ok then
+    return nil
+  else
+    return buf_option
+  end
+end
+
 return utils
