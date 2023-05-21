1f7429cb82884a51e38f4bb440a5b8a455b80428
scarktt <ssoteloserrano@gmail.com>
2023-05-21 13:37:48 -0600

    feat(options): add autocommand for restore cursor position

--- lua/options.lua
+++ lua/options.lua
@@ -51,3 +51,11 @@ vim.cmd [[autocmd FileType lua setlocal shiftwidth=2 tabstop=2]]
 vim.cmd [[autocmd FileType python setlocal expandtab shiftwidth=4 softtabstop=4]]
 vim.cmd [[autocmd FileType php setlocal expandtab shiftwidth=2 softtabstop=2]]
 
+-- Restore cursor position
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+    pattern = { "*" },
+    callback = function()
+        vim.api.nvim_exec('silent! normal! g`"zv', false)
+    end,
+})
+
